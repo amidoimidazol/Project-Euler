15ddd4c97aad4da0f80e1628c40aa5cb3c669112
Peter Forgacs <furamail001@gmail.com>
2016-01-25 10:48:06 +0100

    Solved Problem 11

--- problems.h
+++ problems.h
@@ -5,6 +5,7 @@
 #include <vector>
 #include <string>
 #include <fstream>
+#include <algorithm>
 
 using namespace std;
 
@@ -21,6 +22,180 @@ class P {
 public:
 	P() {
 
+	}
+};
+
+class P11 {
+	/*
+	Problem 11 - Largest product in a grid
+
+	Problem Description:
+	
+	In the 20×20 grid below, four numbers along a diagonal line have been marked in red.
+
+	08 02 22 97 38 15 00 40 00 75 04 05 07 78 52 12 50 77 91 08
+	49 49 99 40 17 81 18 57 60 87 17 40 98 43 69 48 04 56 62 00
+	81 49 31 73 55 79 14 29 93 71 40 67 53 88 30 03 49 13 36 65
+	52 70 95 23 04 60 11 42 69 24 68 56 01 32 56 71 37 02 36 91
+	22 31 16 71 51 67 63 89 41 92 36 54 22 40 40 28 66 33 13 80
+	24 47 32 60 99 03 45 02 44 75 33 53 78 36 84 20 35 17 12 50
+	32 98 81 28 64 23 67 10 26 38 40 67 59 54 70 66 18 38 64 70
+	67 26 20 68 02 62 12 20 95 63 94 39 63 08 40 91 66 49 94 21
+	24 55 58 05 66 73 99 26 97 17 78 78 96 83 14 88 34 89 63 72
+	21 36 23 09 75 00 76 44 20 45 35 14 00 61 33 97 34 31 33 95
+	78 17 53 28 22 75 31 67 15 94 03 80 04 62 16 14 09 53 56 92
+	16 39 05 42 96 35 31 47 55 58 88 24 00 17 54 24 36 29 85 57
+	86 56 00 48 35 71 89 07 05 44 44 37 44 60 21 58 51 54 17 58
+	19 80 81 68 05 94 47 69 28 73 92 13 86 52 17 77 04 89 55 40
+	04 52 08 83 97 35 99 16 07 97 57 32 16 26 26 79 33 27 98 66
+	88 36 68 87 57 62 20 72 03 46 33 67 46 55 12 32 63 93 53 69
+	04 42 16 73 38 25 39 11 24 94 72 18 08 46 29 32 40 62 76 36
+	20 69 36 41 72 30 23 88 34 62 99 69 82 67 59 85 74 04 36 16
+	20 73 35 29 78 31 90 01 74 31 49 71 48 86 81 16 23 57 05 54
+	01 70 54 71 83 51 54 69 16 92 33 48 61 43 52 01 89 19 67 48
+
+	The product of these numbers is 26 × 63 × 78 × 14 = 1788696.
+
+	What is the greatest product of four adjacent numbers in the same direction (up, down, left, right, or diagonally) in the 20×20 grid?
+
+	Possible Improvements:
+
+	Left: at least 4th item in the list -> index 3
+	Right: at least 20 - 4 = 17th item in the list -> index 16
+	Up: at least 4th row
+	Down: max 17th row
+	
+	Diagonal Up left:    at least 4th row   && at least 4th element in the list
+	Diagonal Up Right:   at least 4th row   && at max 17th element in the list
+	Diagonal Down left:  at max 17th row    && at least 4th element in the list
+	Diagonal Down right: at max 17th row    && at max 17th item
+
+	*/
+public:
+	const int adjecent_elements = 4;
+	const int length_of_rows = 20;
+	vector<vector<int>> matrix;
+	unsigned long long int biggest_product = 1;
+	
+
+	P11() {
+		vector<vector<int>> matrix = create_matrix();
+		for (int i = 0; i < length_of_rows; ++i) {
+			for (int j = 0; j < length_of_rows; ++j) {
+		
+				 unsigned long long int k = get_directions(i, j);
+				 if (biggest_product < k) { biggest_product = k; }
+			}
+		}
+		cout << biggest_product << endl;
+	}
+		
+	vector<vector<int>> create_matrix() {
+		// Reading in the numbers into a matrix
+		int num;
+		ifstream file("Database/p11.txt");
+		for (int i = 0; i < length_of_rows; ++i) {
+			vector<int> row;
+			for (int j = 0; j < length_of_rows; ++j) {
+				file >> num;
+				row.push_back(num);
+			}
+			matrix.push_back(row);
+		}
+		return matrix;
+	}
+	
+	unsigned long long int get_directions(int row, int elem) {
+
+		try {
+
+			vector<int> result;
+			result.reserve(8);
+		
+			int sum_left = 1, sum_up = 1, up_left_diagonal = 1, sum_right = 1, up_right_diagonal = 1, sum_down = 1, down_left_diagonal = 1, down_right_diagonal = 1;
+
+			//TODO Create a general method that handles all the possible sides
+			// Left or Up
+			if (elem - adjecent_elements >= 0) {
+				for (int i = 0; i <= adjecent_elements - 1; ++i) {
+					sum_left *= matrix.at(row).at(elem - i);
+
+				}
+				result.push_back(sum_left);
+
+				// Diagonal Up left
+				if (row - adjecent_elements >= 0) {
+					for (int i = 0; i <= adjecent_elements - 1; ++i) {
+						up_left_diagonal *= matrix.at(row - i).at(elem - i);
+					}
+					result.push_back(up_left_diagonal);
+				}
+			}
+
+			// Up 
+			if (row - adjecent_elements >= 0) {
+				for (int i = 0; i <= adjecent_elements - 1; ++i) {
+					sum_up *= matrix.at(row - i).at(elem);
+				}
+				result.push_back(sum_up);
+			}
+
+			// Right
+			if (elem + adjecent_elements <= length_of_rows - 1) {
+				for (int i = 0; i <= adjecent_elements - 1; ++i) {
+					sum_right *= matrix.at(row).at(elem + i);
+				}
+				result.push_back(sum_right);
+
+				// Diagonal Up Right
+				if (row - adjecent_elements >= 0) {
+					for (int i = 0; i <= adjecent_elements - 1; ++i) {
+						up_right_diagonal *= matrix.at(row - i).at(elem + i);
+					}
+					result.push_back(up_right_diagonal);
+				}
+			}
+
+			// Down
+			if (row + adjecent_elements <= length_of_rows - 1) {
+				for (int i = 0; i <= adjecent_elements - 1; ++i) {
+					sum_down *= matrix.at(row + i).at(elem);
+				}
+				result.push_back(sum_down);
+
+				// Diagonal Down Left
+				if (elem - adjecent_elements >= 0) {
+					for (int i = 0; i <= adjecent_elements - 1; ++i) {
+						down_left_diagonal *= matrix.at(row + i).at(elem - i);
+					}
+					result.push_back(down_left_diagonal);
+				}
+
+				// Diagonal Down Right
+				if (elem + adjecent_elements <= length_of_rows - 1) {
+					for (int i = 0; i <= adjecent_elements - 1; ++i) {
+						down_right_diagonal *= matrix.at(row + i).at(elem + i);
+					}
+					result.push_back(down_right_diagonal);
+				}
+			}
+
+			unsigned long long int biggest_product = 1;
+
+			if (result.size() > 0) {
+				// Finding the biggest ones
+				sort(result.begin(), result.end());
+				biggest_product = result.at(result.size() - 1);
+			}
+
+			return biggest_product;
+		}
+
+		catch (...) {
+			cout << row << endl;
+			cout << elem << endl;
+			system("pause");
+		}
 	}
 };
 
@@ -115,7 +290,6 @@ public:
 		// Rounded
 		int max_c = 1000 - 1;
 
-		clock_t runtime = clock();
 		for (int c = max_c; c > 1; --c) {
 			for (int b = 1; b < max_c; ++b) {
 				for (int a = 0; a < max_c - b; ++a) {
@@ -132,8 +306,7 @@ public:
 		}
 
 	end:
-		printf("Full runtime: %.2fs\n", (double)(clock() - runtime) / CLOCKS_PER_SEC);
-		system("pause");
+		cout << "Finished" << endl;
 	}
 };
 
